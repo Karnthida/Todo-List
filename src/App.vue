afca0e1283f13264d3d927479e70d95dc7150adf
Karnthida <karnthida.film@gmail.com>
2022-12-28 18:05:42 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="nav">
+    <h2>TodoList</h2>
+  </div>
   <router-view/>
 </template>
 
 <style>
 #app {
+  padding: 0;
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+<style scoped>
+.nav{
   text-align: center;
-  color: #2c3e50;
+  padding: 0;
+  display: inline;
+  color: grey;
+  text-decoration: underline;
+  text-decoration-color: rgb(16, 109, 185);
+  text-underline-offset: 4px;
 }
 
-nav {
-  padding: 30px;
+.nav h2{
+  margin-top: 50px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
